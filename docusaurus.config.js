d2a5f74d5e3b1aea5c2817d9c1eb18ab85f139f4
suranah <surana.h@gmail.com>
2021-07-24 09:50:33 +0530

    docs: adding logo & favicon placeholders

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon-simple.png',
   organizationName: 'chaos-genius', // Usually your GitHub org/user name.
   projectName: 'chaos_genius', // Usually your repo name.
   themeConfig: {
@@ -17,7 +17,7 @@ module.exports = {
       title: 'ChaosGenius Documentation',
       logo: {
         alt: 'ChaosGenius Logo',
-        src: 'img/logo.svg',
+        src: 'img/logo-og.png',
       },
       items: [
         {
